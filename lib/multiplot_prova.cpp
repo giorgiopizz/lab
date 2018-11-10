d20f40a3d36a27a93078f0317cadc7a076dd4249
philoez98 <40722053+philoez98@users.noreply.github.com>
2018-11-10 13:17:04 +0100

    Aggiunti files per testare la gestione del layout

--- lib/multiplot_prova.cpp
+++ lib/multiplot_prova.cpp
@@ -1,10 +1,15 @@
 //c++ multiplot_prova.cpp multiplot.cc analyzer.cc -o multiplot `root-config --cflags --glibs`
+//*** Ho provato a includere più files (sono il copia/incolla di quelli già esistenti) per verificare l'error checking e
+// il layout dei grafici ***
 #include "multiplot.h"
 
 int main(){
 	vector<string> files;
 	files.push_back("data1.txt");
 	files.push_back("data2.txt");
-	multiplot t(2,files,"counts");
+	files.push_back("data3.txt");
+	files.push_back("data4.txt");
+	//files.push_back("data5.txt");
+	multiplot t(4,files,"counts");
 	t.display();
 }
